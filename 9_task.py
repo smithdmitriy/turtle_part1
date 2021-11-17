d66f463188309c29f109d01e1e86975843fb91fd
smithdmitriy <smithdmitriy@gmail.com>
2021-11-17 20:08:59 +0500

    9 task -

--- 9_task.py
+++ 9_task.py
@@ -1,2 +1,28 @@
-def polygon(n: int):
-    pass
+import turtle as tr
+import numpy as np
+
+
+def polygon(n: int, r:float):
+    tr.shape('turtle')
+    tr.speed(1)
+    an = 2*r*np.sin(np.pi/n)
+    tr.left(180 - 180 / n)
+    for i in range(n):
+        tr.forward(an)
+        print(an)
+        tr.left(360 / n)
+    tr.right(180/n)
+    return
+
+
+r = 30
+step = 30
+
+for i in range(3, 10):
+    polygon(i,r)
+    tr.penup()
+    tr.forward(step)
+    tr.pendown()
+    r += 40
+
+
