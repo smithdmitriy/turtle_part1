b31dac1775c9b98c5f3761a2161cc37c0fd12233
smithdmitriy <smithdmitriy@gmail.com>
2021-11-17 14:29:05 +0500

    add 7 +-

--- 7_task.py
+++ 7_task.py
@@ -1,12 +1,16 @@
-import math
+import numpy as np
 import turtle as tr
 spiral_step = 2
 number_of_spiral_turns = 3
-angle = 0
+angle = 5
 tr.shape('turtle')
-tr.speed(10)
-x = 1
+tr.speed(10000)
+i = 0
+k = 20
+#for i in np.arange(0, 2*np.pi, np.pi/64):
 while True:
-    tr.forward(10 + x)
-    tr.left(360 - x/2*math.pi)
-    x += 1
+    tr.goto(np.sin(i)*k, np.cos(i)*k)
+    i +=0.01
+    k *= 1.001
+   # tr.left(np.cos(x))
+  #  tr.forward(np.cos(i)*10)
