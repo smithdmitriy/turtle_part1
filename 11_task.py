1a1a55225c49e496a1ec83c264e5c909b584d04f
smithdmitriy <smithdmitriy@gmail.com>
2021-11-18 13:03:42 +0500

    11 task add accuracy

--- 11_task.py
+++ 11_task.py
@@ -2,28 +2,26 @@ import turtle as tr
 import numpy as np
 
 def circle(r: float):
+    accuracy = 18
     tr.shape('turtle')
     tr.speed(10)
-    tr.penup()
-    tr.forward(r)
-    tr.pendown()
-    an = 2 * r * np.sin(np.pi / 360)
-    angle_n = 180 - 360 / 360
+    an = 2 * r * np.sin(np.pi / accuracy)
+    angle_n = 180 - 360 / accuracy
     tr.left(180 - angle_n / 2)
-    for side_counter in range(360):
+    for side_counter in range(accuracy):
         tr.forward(an)
-        tr.left(360 / 360)
+        tr.left(360 / accuracy)
     tr.right(180 - angle_n / 2)
-    tr.penup()
-    tr.backward(r)
+
     return
 
 
-r = 20
-step = 15
-number_of_circles_pair = 5
+r = 40
+step = 5
+number_of_circles_pair = 10
 for i in range(number_of_circles_pair):
     circle(r)
+    circle(-r)
     r += step
 
 
