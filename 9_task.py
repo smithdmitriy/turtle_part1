66768b4cb8eaeb4f48545a6ef41369318f76c416
smithdmitriy <smithdmitriy@gmail.com>
2021-11-18 09:14:56 +0500

    9 task final

--- 9_task.py
+++ 9_task.py
@@ -2,27 +2,27 @@ import turtle as tr
 import numpy as np
 
 
-def polygon(n: int, r:float):
+def polygon(n: int, r: float):
     tr.shape('turtle')
-    tr.speed(1)
-    an = 2*r*np.sin(np.pi/n)
-    tr.left(180 - 180 / n)
-    for i in range(n):
+    tr.speed(10)
+    tr.penup()
+    tr.forward(r)
+    tr.pendown()
+    an = 2 * r * np.sin(np.pi / n)
+    angle_n = 180 - 360 / n
+    tr.left(180 - angle_n / 2)
+    for counter in range(n):
         tr.forward(an)
-        print(an)
         tr.left(360 / n)
-    tr.right(180/n)
+    tr.right(180 - angle_n / 2)
+    tr.penup()
+    tr.backward(r)
+
     return
 
 
-r = 30
-step = 30
-
-for i in range(3, 10):
-    polygon(i,r)
-    tr.penup()
-    tr.forward(step)
-    tr.pendown()
-    r += 40
-
-
+radius_circumscribed_circle = 10
+step = 15
+for i in range(3, 13):
+    polygon(i, radius_circumscribed_circle)
+    radius_circumscribed_circle += step
