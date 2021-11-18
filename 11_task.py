47565303dc7f2563a1a3bba630e01f5cec48b829
smithdmitriy <smithdmitriy@gmail.com>
2021-11-18 12:50:09 +0500

    11 task +-

--- 11_task.py
+++ 11_task.py
@@ -1,15 +1,24 @@
 import turtle as tr
+import numpy as np
 
-
-def circle(r):
-    tr.left(90)
-    for i in range(72):
-
-        tr.shape('turtle')
-        tr.speed(20)
-        tr.forward(5)
-        tr.left(5)
+def circle(r: float):
+    tr.shape('turtle')
+    tr.speed(10)
+    tr.penup()
+    tr.forward(r)
+    tr.pendown()
+    an = 2 * r * np.sin(np.pi / 360)
+    angle_n = 180 - 360 / 360
+    tr.left(180 - angle_n / 2)
+    for side_counter in range(360):
+        tr.forward(an)
+        tr.left(360 / 360)
+    tr.right(180 - angle_n / 2)
+    tr.penup()
+    tr.backward(r)
     return
+
+
 r = 20
 step = 15
 number_of_circles_pair = 5
