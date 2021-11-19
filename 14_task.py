7903bb5bd7021988f4b3c2eb610fc04609967597
smithdmitriy <smithdmitriy@gmail.com>
2021-11-19 17:38:35 +0500

    14 task +-

--- 14_task.py
+++ 14_task.py
@@ -3,9 +3,6 @@ import numpy as np
 
 def star(r:float,n: int):
     tr.speed(5)
-    q = 2
-#    an = 2 * np.tan(q * np.pi / n)
-#    angle = (n - 2 * q) / n * 180
     angle = 1 / n * 180
     for i in range(n):
         tr.right(180-angle)
