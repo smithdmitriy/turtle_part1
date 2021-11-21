7d29013b8488402d625ed88265d81e51c0c9d902
smithdmitriy <smithdmitriy@gmail.com>
2021-11-21 21:00:51 +0500

    14 task final (only 5 and 11 stars)

--- 14_task.py
+++ 14_task.py
@@ -1,17 +1,18 @@
 import turtle as tr
 
-def star(r:float,n: int):
+
+def star(a=300, n=5):
     tr.speed(5)
     angle = 1 / n * 180
     for i in range(n):
-        tr.right(180-angle)
-        tr.forward(300)
+        tr.right(180 - angle)
+        tr.forward(a)
 
     return
 
-star(20,5)
+
+star()
 tr.pu()
 tr.forward(350)
 tr.pd()
-star(20,11)
-
+star(300, 11)
